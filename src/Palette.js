509cc51ef55b0b83d0902724b1a2fd2f4e443b7d
Othmane Mahfoud <mahfoud.othmane97@gmail.com>
2023-08-14 17:05:59 +0100

    add color level slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
+
 import ColorBox from './ColorBox';
 import './Palette.css'
 
 class Palette extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            level: 500
+        }
+        this.changeLevel = this.changeLevel.bind(this)
     }
-    render() { 
-        const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name}/>
+
+    changeLevel(level) {
+        this.setState({
+            level
+        })
+    }
+
+    render() {
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[level].map(color => (
+            <ColorBox background={color.hex} name={color.name} />
         ))
-        return ( 
+        return (
             <div className='Palette'>
+                <Slider defaultValue={level} min={100} max={900} onAfterChange={this.changeLevel} step={100} />
                 <div className='Palette-colors'>
                     {colorBoxes}
                 </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Palette;
