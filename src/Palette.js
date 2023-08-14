b62f8526a206a638cd867d9b5ff1e45e26a89d14
Othmane Mahfoud <mahfoud.othmane97@gmail.com>
2023-08-14 14:04:00 +0100

    created react-app and added palette

---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() { 
+        return ( 
+            <div className='Palette'>
+                <div className='Palette-colors'>
+                    Palette
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Palette;
