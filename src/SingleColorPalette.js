db320cacf2c7cc5921fcf0ccbb034e6e3e0273d5
Othmane Mahfoud <mahfoud.othmane97@gmail.com>
2023-08-18 12:59:32 +0100

    add go back boc and change text color depending on luminance

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+import { Link } from 'react-router-dom';
 
 class SingleColorPalette extends Component {
     constructor(props) {
@@ -32,23 +33,31 @@ class SingleColorPalette extends Component {
 
     render() {
         const { format } = this.state
-        const { paletteName, emoji } = this.props.palette
+        const { paletteName, emoji, id } = this.props.palette
         const colorBoxes = this._shades.map(color => (
             <ColorBox
-                key={color.id}
+                key={color.name}
                 name={color.name}
                 background={color[format]}
                 showLink={false}
             />
         ))
         return (
-            <div className='Palette'>
+            <div className='SingleColorPalette Palette'>
                 <Navbar
                     handleChange={this.changeFormat}
                     showingAllColors={false}
                 />
                 <div className='Palette-colors'>
                     {colorBoxes}
+                    <div className='go-back ColorBox'>
+                        <Link
+                            className='back-button'
+                            to={`/palette/${id}`}
+                        >
+                            Go Back
+                        </Link>
+                    </div>
                 </div>
                 <PaletteFooter
                     paletteName={paletteName}
